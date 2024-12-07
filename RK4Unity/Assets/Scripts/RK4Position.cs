cf75519a97d8f66cf72a8db722459e4eb45670e9
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2024-12-07 13:52:18 -0500

    adding rk4

--- RK4Unity/Assets/Scripts/RK4Position.cs
+++ RK4Unity/Assets/Scripts/RK4Position.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 
 public class RK4Position : MonoBehaviour
 {
-    public void Slope()
-    {
-        //the equasion that perdicts movment
-    }
-    private void UpdatePosition(Action Slope)
-    {
-        //do runga kutta based on given slope function
-    }
+    [SerializeField]
+    private float velocity = 1000;
+    private Vector2 direction = new Vector2(1, 1);
 
     private void FixedUpdate()
     {
-        UpdatePosition(Slope);
+        RK4();
     }
+
+    private void RK4()
+    {
+       
+        float k1x = velocity * Time.deltaTime * direction.x;
+        float k1y = velocity * Time.deltaTime * direction.y;
+
+        
+        float k2x = velocity * (k1x * Time.deltaTime / 2) * direction.x;
+        float k2y = velocity * (k1y * Time.deltaTime / 2) * direction.x;
+
+        
+        float k3x = velocity * (k2x * Time.deltaTime / 2) * direction.x;
+        float k3y = velocity * (k2y * Time.deltaTime / 2) * direction.x;
+        
+        float k4x = velocity * Time.deltaTime * direction.x * k3x;
+        float k4y = velocity * Time.deltaTime * direction.y * k3y;
+  
+
+        float finalX = gameObject.transform.position.x + Time.deltaTime / 6 * (k1x + 2 * k2x + 2 * k3x + k4x);
+        float finalY = gameObject.transform.position.y + Time.deltaTime / 6 * (k1y + 2 * k2y + 2 * k3y + k4y);
+
+        gameObject.transform.position = new Vector2(finalX, finalY);
+    }
+
+
+
 }
